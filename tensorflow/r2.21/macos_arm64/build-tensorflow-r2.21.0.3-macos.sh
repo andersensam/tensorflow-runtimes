98f3043a2c5545f941efd6783927bdbaca82520b
Samuel Andersen <samstext@gmail.com>
2026-06-17 10:13:21 -0700

    Add build script for macOS

---
+++ tensorflow/r2.21/macos_arm64/build-tensorflow-r2.21.0.3-macos.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Setup a temporary directory for building TensorFlow
+mkdir build
+
+# Ensure Python 3.12 is already installed and is accessible
+python3.12 -m venv build/.venv
+source build/.venv/bin/activate
+pip install --upgrade pip uv
+
+# Fetch the bazel binary
+curl -o build/bazel https://storage.googleapis.com/axlearn-wheels/bazelisk/v1.29.0/bazelisk-darwin-arm64
+chmod +x build/bazel
+
+# Clone TensorFlow
+cd build && git clone --depth 1 --branch r2.21 https://github.com/andersensam/tensorflow
+
+# Generate the .tf_configure.bazelrc for building TensorFlow
+echo "build --action_env PYTHON_BIN_PATH=\"$(pwd)/.venv/bin/python3.12\"" > tensorflow/.tf_configure.bazelrc
+echo "build --action_env PYTHON_LIB_PATH=\"$(pwd)/.venv/lib/python3.12/site-packages\"" >> tensorflow/.tf_configure.bazelrc
+echo "build --python_path=\"$(pwd)/.venv/bin/python3.12\"" >> tensorflow/.tf_configure.bazelrc
+
+# Copy the rest of the template .tf_configure.bazelrc
+cat ../tf_r2.21.0.3_macos_arm64.brc >> tensorflow/.tf_configure.bazelrc
+
+# Start building TensorFlow
+export HERMETIC_PYTHON_VERSION=3.12
+cd tensorflow && ../bazel build //tensorflow/tools/pip_package:wheel --repo_env=WHEEL_NAME=tensorflow --config=macos_arm64 \
+    --verbose_failures --define=no_system_libs=true --action_env TF_SYSTEM_LIBS="" --spawn_strategy=darwin-sandbox
